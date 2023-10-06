6f953921f7d28c91c98a02ca1d21f7e42e4ade62
enhatl <emily@hollingshead.org>
2023-10-06 09:25:49 -0500

    switched to ipynb file

--- main-proj.py
+++ main-proj.py
@@ -1,1 +1,5 @@
 # https://huggingface.co/datasets/maharshipandya/spotify-tracks-dataset/tree/main
+import pandas as pd
+url = 'https://raw.githubusercontent.com/enhatl/ML-Semester-Proj/main/dataset.csv'
+df = pd.read_csv(url,index_col=0,parse_dates=[0])
+
